0f731f52a68715a5bfb31741861fd891b038a90f
samr55 <samruddhibhilarkar5@gmail.com>
2025-08-21 23:04:11 +0530

    Script.sh

--- Script2.sh
+++ Script2.sh
@@ -6,5 +6,4 @@ echo "Value of b"
 read b
 echo "Hello value of a is $a and value of b is $b"
 echo "Performing my practical first time"
-echo " good Morning"
-echo " Good Night"
+echo " Value of a b"
